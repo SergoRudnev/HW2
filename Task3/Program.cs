bbfa6c9af2b79326d159212d0cf1bafa14b8b6c3
Sergey Rudnev <sergorudnev@gmail.com>
2022-09-23 01:54:11 +0500

    Task3 Done

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+int CreateNamber()
+{
+    int number = new Random().Next(1,8);
+    return number;
+}
+int a = CreateNamber();
+if(a>5)
+{
+    Console.WriteLine(a);
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine(a);
+    Console.WriteLine("Будний день");
+}
