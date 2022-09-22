784da2560a5ad66718821a09712a8df253bbf2e7
Sergey Rudnev <sergorudnev@gmail.com>
2022-09-23 01:16:09 +0500

    Task1 Done

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+int CreateNamber()
+{
+    int number = new Random().Next(100,1000);
+    return number;
+}
+int a = CreateNamber();
+int SecondNumber = a % 100 / 10;
+Console.WriteLine(a);
+Console.WriteLine(SecondNumber);
